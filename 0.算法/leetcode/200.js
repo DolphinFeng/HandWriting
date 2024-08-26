878dad8f3f929ea5370c25532a658c24a4df7ae3
fengduping <fengduping@meituan.com>
2024-08-26 20:28:09 +0800

    feat: 0826(二)

---
+++ 0.算法/leetcode/200.js
@@ -0,0 +1,84 @@
+// 岛屿数量
+
+// 给你一个由 '1'（陆地）和 '0'（水）组成的的二维网格，请你计算网格中岛屿的数量。
+
+// 岛屿总是被水包围，并且每座岛屿只能由水平方向和/或竖直方向上相邻的陆地连接形成。
+
+// 此外，你可以假设该网格的四条边均被水包围。
+
+ 
+
+// 示例 1：
+
+// 输入：grid = [
+//   ["1","1","1","1","0"],
+//   ["1","1","0","1","0"],
+//   ["1","1","0","0","0"],
+//   ["0","0","0","0","0"]
+// ]
+// 输出：1
+// 示例 2：
+
+// 输入：grid = [
+//   ["1","1","0","0","0"],
+//   ["1","1","0","0","0"],
+//   ["0","0","1","0","0"],
+//   ["0","0","0","1","1"]
+// ]
+// 输出：3
+
+
+function countIslands(grid) {
+    var numIslands = function(grid) {
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
+
+        let numIslands = 0;
+
+        for (let i = 0; i < grid.length; i++) {
+            for (let j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == '1') {
+                    numIslands += 1;
+                    dfs(grid, i, j);
+                }
+            }
+        }
+
+        return numIslands;
+    };
+
+    var dfs = function(grid, i, j) {
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == '0') {
+            return;
+        }
+
+        grid[i][j] = '0';
+
+        dfs(grid, i + 1, j);
+        dfs(grid, i - 1, j);
+        dfs(grid, i, j + 1);
+        dfs(grid, i, j - 1);
+    };
+
+    return numIslands(grid);
+}
+
+// 示例调用
+let grid1 = [
+  ["1","1","1","1","0"],
+  ["1","1","0","1","0"],
+  ["1","1","0","0","0"],
+  ["0","0","0","0","0"]
+];
+
+let grid2 = [
+  ["1","1","0","0","0"],
+  ["1","1","0","0","0"],
+  ["0","0","1","0","0"],
+  ["0","0","0","1","1"]
+];
+
+console.log(countIslands(grid1)); // 输出: 1
+console.log(countIslands(grid2)); // 输出: 3
+

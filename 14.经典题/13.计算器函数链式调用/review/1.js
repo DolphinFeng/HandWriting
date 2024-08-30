e7a6dba70b5674fed1c67024fe0e1832895a5672
fengduping <fengduping@meituan.com>
2024-08-30 11:34:25 +0800

    feat: 0830

---
+++ 14.经典题/13.计算器函数链式调用/review/1.js
@@ -0,0 +1,36 @@
+class Chain {
+    constructor (num) {
+        this.num = num
+    }
+
+    add (num) {
+        this.num += num
+        return this
+    }
+
+    subtract (num) {
+        this.num -= num
+        return this
+    }
+
+    multiply (num) {
+        this.num *= num
+        return this
+    }
+
+    divide (num) {
+        if (num === 0) {
+            console.error('error');
+        } else {
+            this.num /= num
+        }
+        return this
+    }
+
+    getValue () {
+        return this.num
+    }
+}
+
+const compute = new Chain(10)
+console.log(compute.add(5).subtract(3).multiply(2).divide(0).getValue());

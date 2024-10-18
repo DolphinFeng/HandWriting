12d264e18285812d1ae89b9b3e05c3b46d585fc9
DolphinFeng <2292241828@qq.com>
2024-10-18 16:27:03 +0800

    feat: 重构(二)

--- 2.防抖节流/review/debounce.js
+++
@@ -1,9 +0,0 @@
-function debounce (fn, wait) {
-    let timer
-    return function () {
-        if (timer) clearTimeout(timer)
-        timer = setTimeout(() => {
-            fn.apply(this, arguments)
-        }, wait)
-    }
-}
